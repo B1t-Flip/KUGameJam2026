d134836016c085eba9867d41275827570658b059
Vlad Ciobanu <jumiciobi@gmail.com>
2026-02-19 23:49:42 +0000

    pedestal prefabbed

---
+++ Assets/Code/Player/SoundManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+public class SoundManager : MonoBehaviour {
+  [SerializeField] private AudioSource[] 
+  
+  public enum SoundType {
+    DOOR_CLOSE, DOOR_OPEN, JUMP, LAND, 
+    PICKUP, PRESSURE_PLATE, SPLASH, STEP, 
+    UI_BACK, UI_HOVER, UI_SELECT
+  }
+  public delegate void PlaySound(SoundType type);
+  public static event PlaySound OnSound;
+
+  private void OnEnable() => OnSound += PlayASound;
+  private void OnDisable() => OnSound += PlayASound;
+  
+
+  private void PlayASound(SoundType type) {
+    
+  }
+}
